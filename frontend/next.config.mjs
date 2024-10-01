2cbbd79dc784a08479706724785189d4f466879c
WANDERLEY DE SOUZA MACHADO <2008.wsm@gmail.com>
2024-09-30 23:06:57 -0300

    Criando projeto frontend - pagina principal

---
+++ frontend/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+        ],
+    },
+}
+export default nextConfig;
---
+++ frontend/src/components/produto/ListaProdutos.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { produtos } from '@/core'
+import useProdutos from '@/data/hooks/useParcelamento'
+import ProdutoItem from './ProdutoItem'
+import ProdutoNaoEncontrado from './ProdutoNaoEncontrado'
+
+export default function ListaProdutos() {
+    // const { produtos } = useProdutos()
+    return produtos.length ? (
+        <div
+            className="
+                grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
+            "
+        >
+            {produtos.map((produto) => (
+                <ProdutoItem produto={produto} key={produto.id} />
+            ))}
+        </div>
+    ) : (
+        <ProdutoNaoEncontrado semBotaVoltar />
+    )
+}
